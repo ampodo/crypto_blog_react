91464650ca03715e13070271d6be899b0e35a1ac
ampodo <89546097+ampodo@users.noreply.github.com>
2022-04-05 00:21:51 +0300

    Update index.js

--- components/index.js
+++ components/index.js
@@ -9,4 +9,5 @@ export { default as Comments } from './Comments';
 export { default as PostDetail } from './PostDetail';
 export { default as FeaturedPostCard } from './FeaturedPostCard';
 export { default as Loader } from './Loader';
-
+export { default as useRightClickMenu } from './useRightClickMenu';
+export { default as Menu } from './Menu';
