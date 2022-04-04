8817ad25497d1774bc03baa688d14a068dac1710
ampodo <89546097+ampodo@users.noreply.github.com>
2022-04-05 00:18:19 +0300

    Create Menu.js

---
+++ components/Menu.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Menu = (x,y, showMenu) => {
+    const style = () => {
+           return {
+                  
+                
+        };
+    };
+     return (
+        <div style={style()}>
+            
+        </div>    
+  );
+};
+
+  const style = {
+       
+      div: {
+             
+          },
+          margin: {
+              
+          }
+       }
+
+export default Menu
